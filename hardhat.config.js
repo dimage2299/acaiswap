eaa74a32ee349f866a66aaaae9ba133faab8d073
dimage2299 <86098760+dimage2299@users.noreply.github.com>
2022-04-21 14:29:04 -0400

    Add files via upload

---
+++ hardhat.config.js
@@ -0,0 +1,55 @@
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-waffle");
+require("hardhat-deploy");
+require("hardhat-deploy-ethers");
+require("solidity-coverage");
+require('dotenv').config();
+require("@nomiclabs/hardhat-etherscan");
+
+
+ module.exports = {
+  networks: {
+    hardhat: {
+      // comment out for local testing
+      // uncomment for fork script
+      // forking: {
+      //   url: "https://eth-mainnet.alchemyapi.io/v2/xvCrVTNc-KptCs44ACV1IyaLxcjWasuI",
+      //   enabled: false,
+      // },
+    },
+    rinkeby: {
+      url: process.env.ALCHEMY_RINKEBY_URL,
+      accounts: [process.env.DEV_PRIVATE_KEY],
+    },
+    tevmos: {
+      url: "https://eth.bd.evmos.dev:8545",
+      accounts: [process.env.DEV_PRIVATE_KEY]
+    }
+  },
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.0",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+      {
+        version: "0.6.12",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    ],
+  },
+  etherscan: {
+    mainnet: process.env.ETHERSCAN_KEY,
+    evmosTestnet: process.env.ETHERSCAN_KEY
+  }
+};
